3b42b34d1a61f9005d19bdff03f0e3bd25b18819
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-05-16 00:15:35 +0200

    feat(Ficha10):Exercicio 01 Done.

---
+++ FichasDeExercicios/Ficha10/Aula_Pratica/Exercicio01ArrayFactorial.java
@@ -0,0 +1,32 @@
+package FichasDeExercicios.Ficha10.Aula_Pratica;
+/*
+* Criar um array do tipo Double com 150 posicoes, Calcular factorial de cada posicao
+* e guardar o valor no respectivo elemento correspondente.*/
+public class Exercicio01ArrayFactorial {
+    public static void main(String[] args) {
+        // Declaracao do array
+        double [] factoriais = new double[150];
+        // Preencher o array com os valores factoriais
+        for(int i=0; i<factoriais.length;i++){
+            factoriais[i] = calcularFactorial(i);
+        }
+        // Saida de Dados: Alguns valores
+        System.out.println("Factorial de 0: " + factoriais[0]);
+        System.out.println("Factorial de 1: " + factoriais[1]);
+        System.out.println("Factorial de 2: " + factoriais[2]);
+        System.out.println("Factorial de 5: " + factoriais[5]);
+    }
+
+    // Metodo para Calcular Factorial de um Numero
+    public static double calcularFactorial(int n){
+        if (n == 0){
+            return 0;
+        }
+
+        double resultado = 1;
+        for(int i=1; i <= n; i++){
+            resultado *= i;
+        }
+        return resultado;
+    }
+}
---
+++ FichasDeExercicios/Ficha10/Aula_Teorica/Exemplo03.java
@@ -0,0 +1,18 @@
+/*package FichasDeExercicios.Ficha10.Aula_Teorica;
+
+import java.util.Vector;
+
+public class Exemplo03 {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        Float f = new Float(32.0f);
+        v.addElement(f);
+        Integer i = new Integer (12);
+        v.addElement (i);
+        Float f = (Float) v.elementAt (0);
+        Integer i = (Integer) v.elementAt (1);
+        System.out.println ("v [0] = "+ f.toString ());
+        System.out.println ("v [1] = "+ i.toString ());
+        // Tirar duvida com docente
+    }
+}*/
