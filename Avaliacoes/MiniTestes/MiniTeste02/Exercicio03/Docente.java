816d541426eb7b7a1498daf142d9ed3e8ee01829
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-05-19 09:46:53 +0200

    feat(MiniTeste02_Exercicio03): calcSalario method Done.

--- Avaliacoes/MiniTestes/MiniTeste02/Exercicio03/Docente.java
+++ Avaliacoes/MiniTestes/MiniTeste02/Exercicio03/Docente.java
@@ -10,7 +10,38 @@ public class Docente extends Funcionario{
 
     @Override
     public double calcularSalario(char cat, int nrDep, double sal) {
-        return 0;
+        if(horas <= 0) {
+            // Docente a tempo inteiro
+            switch (cat) {
+                case 'A':
+                    sal = 15200;
+                    break;
+                case 'B':
+                    sal = 10500;
+                    break;
+                default:
+                    sal = 0;
+                    break;
+            }
+        }else{
+            // Docente a tempo parcial
+            switch (cat){
+                case 'A':
+                    sal = 750*horas;
+                    break;
+                case 'B':
+                    sal = 470*horas;
+                    break;
+                default:
+                    sal = 0;
+                    break;
+            }
+        }
+        if (nrDep > 5) {
+            return sal + (sal * 0.05);
+        }else{
+            return 0;
+        }
     }
 
     @Override
